8ad7710bd7a8a60857c975eaaf6ec8bb81d442a8
Alex <alex.rus11111@gmail.com>
2022-12-17 14:28:23 +0200

    alezx

--- AM_DanceStudio/Controllers/ClassesController.cs
+++ AM_DanceStudio/Controllers/ClassesController.cs
@@ -1,4 +1,6 @@
 ﻿using AM_DanceStudio.Data;
+using AM_DanceStudio.Models;
+using Humanizer;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -20,6 +22,54 @@ namespace AM_DanceStudio.Controllers
             ViewBag.Classes = classes;
 
             return View();
+        }
+        //Se afiseaza un singur articol in functie de id-ul sau
+
+        public IActionResult Show(int id)
+        {
+            Class classe = db.Classes.Include("Instructor").Include("Style").Include("Studio")
+                            .Where(art => art.Id == id).First();
+
+            ViewBag.Class = classe;
+            ViewBag.Style = classe.Style;
+            ViewBag.Instructor = classe.Instructor;
+            ViewBag.Studio = classe.Studio;
+            return View();
+        }
+
+        //Se afiseaza formularul in care se vor completa datele unei clase
+        //Impreuna cu selectarea stilului, studioului si instructorului care o va tine
+
+        public IActionResult New() {
+            var styless = from styles in db.Styles
+                         select styles;
+            ViewBag.Styles = styless;
+
+            var instructorss = from instructors in db.Instructors
+                               select instructors;
+            ViewBag.Instructors = instructorss;
+
+
+            var studioss = from studios in db.Studios
+                           select studios;
+            ViewBag.Studios = studioss;
+            return View();
+        }
+
+      [HttpPost]
+
+        public IActionResult New(Class clasa)
+        {
+            try
+            {
+                db.Classes.Add(clasa);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            catch (Exception)
+            {
+                return RedirectToAction("New");
+            }
         }
     }
 }
