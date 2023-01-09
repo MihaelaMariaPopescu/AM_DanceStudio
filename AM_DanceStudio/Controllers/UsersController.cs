1670f261091d75a3b65f3e2f15275b3e35d9dac1
Alex <alex.rus11111@gmail.com>
2023-01-10 00:46:44 +0200

    useri

--- AM_DanceStudio/Controllers/UsersController.cs
+++ AM_DanceStudio/Controllers/UsersController.cs
@@ -3,6 +3,7 @@ using AM_DanceStudio.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using System.Data;
 
@@ -80,6 +81,79 @@ namespace AM_DanceStudio.Controllers
             db.SaveChanges();
 
             return RedirectToAction("Index");
+        }
+
+
+        public async Task<ActionResult> Edit(string id)
+        {
+            ApplicationUser user = db.Users.Find(id);
+
+            user.AllRoles = GetAllRoles();
+
+            var roleNames = await _userManager.GetRolesAsync(user); 
+
+            
+            var currentUserRole = _roleManager.Roles
+                                              .Where(r => roleNames.Contains(r.Name))
+                                              .Select(r => r.Id)
+                                              .First(); 
+            ViewBag.UserRole = currentUserRole;
+
+            return View(user);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Edit(string id, ApplicationUser newData, [FromForm] string newRole)
+        {
+            ApplicationUser user = db.Users.Find(id);
+
+            user.AllRoles = GetAllRoles();
+
+
+            if (ModelState.IsValid)
+            {
+                user.UserName = newData.UserName;
+                user.Email = newData.Email;
+                user.FirstName = newData.FirstName;
+                user.LastName = newData.LastName;
+                user.PhoneNumber = newData.PhoneNumber;
+
+
+             
+                var roles = db.Roles.ToList();
+
+                foreach (var role in roles)
+                {
+                    
+                    await _userManager.RemoveFromRoleAsync(user, role.Name);
+                }
+              
+                var roleName = await _roleManager.FindByIdAsync(newRole);
+                await _userManager.AddToRoleAsync(user, roleName.ToString());
+
+                db.SaveChanges();
+
+            }
+            return RedirectToAction("Index");
+        }
+
+        [NonAction]
+        public IEnumerable<SelectListItem> GetAllRoles()
+        {
+            var selectList = new List<SelectListItem>();
+
+            var roles = from role in db.Roles
+                        select role;
+
+            foreach (var role in roles)
+            {
+                selectList.Add(new SelectListItem
+                {
+                    Value = role.Id.ToString(),
+                    Text = role.Name.ToString()
+                });
+            }
+            return selectList;
         }
 
     }
