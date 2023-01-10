57de0f56d5e60a9d85d1a2a3c1ca09c3547ec798
mihaela <mihaela.popescu360@gmail.com>
2023-01-10 02:31:27 +0200

    design

--- AM_DanceStudio/Program.cs
+++ AM_DanceStudio/Program.cs
@@ -54,7 +54,7 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Classes}/{action=Index}/{id?}");
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 app.MapRazorPages();
 
 
